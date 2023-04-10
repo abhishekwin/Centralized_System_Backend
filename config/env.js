3d6767f9c0249782c5b4ad9105509cf5fc320c0a
nayanthoughtwin31 <nayan@thoughtwin.com>
2023-04-10 14:22:18 +0530

    Updated ENV

--- config/env.js
+++ config/env.js
@@ -1,4 +1,19 @@
 const env = {
-    mongoAtlasUri: "mongodb://localhost:27017/centralizedSystem"
-}
+  mongoAtlasUri: "mongodb://localhost:27017/centralizedSystem",
+  TWILIO_ACCOUNT_SID: "AC15daebba7b4d9d8f67a4db3ed126671b",
+  TWILIO_AUTH_TOKEN: "e9ef0b634756797c00f4ae4b4e6ffa59",
+  NODEMAILER_GMAIL: "testthoughtwin@gmail.com",
+  NODEMAILER_PASSWORD: "xlvignquhlvgwhkj",
+  JWT_SECRET: "W@W@!WSAD",
+  adharRequestOtpUri: "https://api.emptra.com/aadhaarVerification/requestOtp",
+  KYCSecretKey:
+    "YBBLW4WXw1QYlNHQd5s2JKXOaka2zJTRmgTE3pT6lDmH9TxsoNqqTCSxd0faqTvLy",
+  KYCClientId:
+    "f82c585638fff825f86d651d6867db13:c2f30ee2795f17fdf1aa536f78b8a789",
+  adharSumitOtpUri: "https://api.emptra.com/aadhaarVerification/submitOtp",
+  XPUB:
+    "xpub6DnSaWsS1kBgww9FDUf2vbKWYwVrYv4SH8QqZmqtjLK8acQ5vbmVUnPyiYjGLJUttrmZL4UyPKDAJtKt4zPLLmEZyFoSnqRS7EYtppQPhj3",
+  MNEMONIC:
+    "reform antique small wet demand way rail suffer favorite okay kit cause detect cake retreat stone struggle regular whisper blind else enrich better useful",
+};
 module.exports = env;
