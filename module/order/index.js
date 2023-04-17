2739d61a0dc0f73f3abb5c54b0094506bf33c41f
nayanthoughtwin31 <nayan@thoughtwin.com>
2023-04-17 19:08:07 +0530

    Updated Order Controllers

--- module/order/index.js
+++ module/order/index.js
@@ -4,23 +4,23 @@ const orderController = require("./controller/order");
 const Authentication = require("../../utility/functions");
 
 //@desc API for creating Buy P2P Orders user can create new buy order using this API
-//@route order/createBuyOrder
-//@access public
+//@route order/createBuyPost
+//@access private
 router.post(
-  "/createBuyOrder",Authentication.userAuthentication, orderController.createBuyOrder
+  "/createBuyPost",Authentication.userAuthentication, orderController.createBuyPost
 );
 //@desc API for creating Sell P2P Orders user can create new sell order using this API
-//@route order/createSellOrder
-//@access public
-router.post("/createSellOrder",Authentication.userAuthentication,orderController.createSellOrder);
+//@route order/createSellPost
+//@access private
+router.post("/createSellPost",Authentication.userAuthentication,orderController.createSellPost);
 //@desc API for getting the buy order details user can get details about the  buy order
-//@route order/getBuyOrder
-//@access public
-router.post("/getBuyOrder", Authentication.userAuthentication, orderController.getBuyOrder);
+//@route order/getBuyPosts
+//@access private
+router.post("/getBuyPosts", Authentication.userAuthentication, orderController.getBuyPosts);
 //@desc API for getting the sell order details user can get details about the sell order
-//@route order/getSellOrder
-//@access public
-router.get("/getSellOrder", Authentication.userAuthentication, orderController.getSellOrder);
+//@route order/getSellPosts
+//@access private
+router.get("/getSellPosts", Authentication.userAuthentication, orderController.getSellPosts);
 
 
 module.exports = router;
