47cc632dc4be3240cd5bdd9f715274d80fc9ad23
nayanthoughtwin31 <nayan@thoughtwin.com>
2023-04-19 12:14:24 +0530

    Updated Index.js

--- module/order/index.js
+++ module/order/index.js
@@ -1,26 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const orderController = require("./controller/order");
+const postController = require("./controller/posts");
+// const orderController = require("./controller/order");
 const Authentication = require("../../utility/functions");
 
 //@desc API for creating Buy P2P Orders user can create new buy order using this API
 //@route order/createBuyPost
 //@access private
 router.post(
-  "/createBuyPost",Authentication.userAuthentication, orderController.createBuyPost
+  "/createBuyPost",Authentication.userAuthentication, postController.createBuyPost
 );
 //@desc API for creating Sell P2P Orders user can create new sell order using this API
 //@route order/createSellPost
 //@access private
-router.post("/createSellPost",Authentication.userAuthentication,orderController.createSellPost);
+router.post("/createSellPost",Authentication.userAuthentication,postController.createSellPost);
 //@desc API for getting the buy order details user can get details about the  buy order
 //@route order/getBuyPosts
 //@access private
-router.post("/getBuyPosts", Authentication.userAuthentication, orderController.getBuyPosts);
+router.post("/getBuyPosts", Authentication.userAuthentication, postController.getBuyPosts);
 //@desc API for getting the sell order details user can get details about the sell order
 //@route order/getSellPosts
 //@access private
-router.get("/getSellPosts", Authentication.userAuthentication, orderController.getSellPosts);
+router.post("/getSellPosts", Authentication.userAuthentication, postController.getSellPosts);
+
+//@desc API for getting the order details based on userId 
+//@route order/getOrderDetails
+//@access private
+router.get("/getOrder/:id", Authentication.userAuthentication, postController.getorderById);
 
 
+//@desc API for Buy order
+//@route order/buyOrder
+//@access private
+// router.post("/buyOrder", Authentication.userAuthentication, orderController.buyOrder);
 module.exports = router;
