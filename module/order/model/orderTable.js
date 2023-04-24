bf638d3ce3ca239732f84d2c6e913b8cba36362c
nayanthoughtwin31 <nayan@thoughtwin.com>
2023-04-24 17:15:33 +0530

    Updated Order Table

---
+++ module/order/model/orderTable.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+let orderTable = mongoose.Schema({
+  postId: { type: mongoose.Schema.Types.ObjectId, ref: "postTable" },
+  postedUserId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  createdby: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  paymentId: { type: mongoose.Schema.Types.ObjectId, ref: "Payment" },
+  cryptoBuyUser: { type: String, default: "" },
+  cryptoSellUser: { type: String, default: "" },
+  amount: { type: Number, default: 0 },
+  quantity: { type: Number, default: 0 },
+  paymentMethod: { type: String, default: "" },
+  orderStatus: {
+    type: String,
+    enum: [
+      "OrderNotCreated",
+      "OrderCreated",
+      "OrderPending",
+      "OrderCompleted",
+      "NotifiyBuyerMoneySent",
+      "NotifiySellerMoneySent",
+      "PaymentSentBuyer",
+      "PaymentSentSeller",
+      "Cancelled",
+      "Pending",
+    ],
+    default: "OrderNotCreated",
+  },
+  buyerUPID: { type: String, default: "" },
+  sellerUPID: { type: String, default: "" },
+});
+
+module.exports = mongoose.model("OrderTable", orderTable);
